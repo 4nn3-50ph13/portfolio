dfc1790f13e1844a166974fb955cd622ffe8deb7
Anne-Sophie Cusson <64850625+4nn3-50ph13@users.noreply.github.com>
2022-01-17 20:40:12 -0500

    Second commit - fix translations

--- src/components/LangBar/index.js
+++ src/components/LangBar/index.js
@@ -4,6 +4,8 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
+import translationEN from "../../assets/locales/en/translation.json";
+import translationFR from "../../assets/locales/fr/translation.json";
 
 
 const LangBar = ({isOpen, toggle}) => {
@@ -11,7 +13,25 @@ const LangBar = ({isOpen, toggle}) => {
         i18n.changeLanguage(code);
         toggle();
     }
-
+    const resources = {
+     en: {
+       translation: translationEN,
+     },
+     fr: {
+       translation: translationFR,
+     },
+    };
+    
+    i18n
+    .use(initReactI18next)
+    .use(LanguageDetector)
+    .use(HttpApi)
+    .init({
+        lng: "en",
+        fallbackLng: "en",
+        resources,
+        react: {useSuspense: false}
+    });
     return (
         <LangContainer isOpen={isOpen} onClick={toggle}>
             <Icon onClick={toggle}>
@@ -24,17 +44,5 @@ const LangBar = ({isOpen, toggle}) => {
         </LangContainer>
     )
 }
-i18n
-.use(initReactI18next)
-.use(LanguageDetector)
-.use(HttpApi)
-.init({
-    lng: "en",
-    fallbackLng: "en",
-    backend: {
-        loadPath: '/assets/locales/{{lng}}/translation.json'
-    },
-    react: {useSuspense: false}
-});
 
 export default LangBar;
